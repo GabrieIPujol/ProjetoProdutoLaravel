cf79f0566c6d4dbc5360616f241683ec936bd394
GABRIEL PUJOL AMARAL GURGEL VELOSA <gabriel.pagvelosa@senacsp.edu.br>
2026-04-13 22:00:56 -0300

    Aula 13/04

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/product/create', [ProductController::class, 'create']);
+    Route::post('/product/store', [ProductController::class, 'store']);
+
+    Route::get('/category/create', [CategoryController::class, 'create']);
+    Route::post('/category/store', [CategoryController::class, 'store']);
+    Route::get('/category/show/{category}', [CategoryController::class, 'show']);
+
+    Route::get('/tag/create', [TagController::class, 'create']);
+    Route::post('/tag/store', [TagController::class, 'store']);
 });
+    Route::get('/product', [ProductController::class, 'index']);    
+    Route::get('/product/show/{product}', [ProductController::class, 'show']);
+    Route::get('/category', [CategoryController::class, 'index']);
+    Route::get('/tag', [TagController::class, 'index']);
 
 require __DIR__.'/auth.php';
